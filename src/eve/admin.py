fa686d5d1ca5ea79541c89f06de30865b653c82c
MACROSSBR <32556207+MACROSSBR@users.noreply.github.com>
2019-02-13 15:01:09 -0200

    Add files via upload

---
+++ src/eve/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import eve
+
+admin.site.register(eve)
+
+# Register your models here.
---
+++ src/eve/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class eve(models.Model):
+    title = models.TextField()
+
+
+
+
+
